c38199942a0247e6cad238cab320bf0654ef6889
vbhone <1124920238@qq.com>
2020-06-09 13:59:15 +0800

    美化界面

--- app/views.py
+++ app/views.py
@@ -17,24 +17,16 @@ def predict(request):
     # input_file = "/static/images/" + path_file_number + ".jpg"
     image=Img(img_url=image)
     image.save()
-    #
-    # image.save(input_file)
-    # input_file=image.img_url.url
-    # input_file="/static/images/about-img.jpg"
     print(image.img_url)
     r_image = yolo.detect_image(Image.open(image.img_url.url))
 
-    # r_image.show()
     path_file_number = str(len(glob.glob(pathname='./app/static/out/*.jpg')) + 1)
-    # print("filename  ", path_file_number)
     filename="./app/static/out/" + path_file_number + ".jpg"
     r_image.save(filename)
     retfile="static/out/" + path_file_number + ".jpg"
     input_file=image.img_url.url.replace("app/static","static")
-    # context.append(input_file)
     context={
         "img":input_file,
         "predict":retfile
     }
-    # yolo.close_session()
     return render(request,"index.html",context)
